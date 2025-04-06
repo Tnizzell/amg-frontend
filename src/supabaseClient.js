35cdb61c349655672a9d1dd49072e15d33fb2137
Tnizzell <tnizzell@hotmail.com>
2025-04-06 13:54:27 -0400

    Frontend updates: added transcribe file and cleaned  up  subabase function

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -5,7 +5,3 @@ const supabaseKey = process.env.REACT_APP_SUPABASE_ANON_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
-
-console.log("SUPABASE_URL:", process.env.REACT_APP_SUPABASE_URL);
-console.log("SUPABASE_KEY:", process.env.REACT_APP_SUPABASE_ANON_KEY);
-
